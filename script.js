72aecde15cf8a9fe8456902bea26d14aec42fdc6
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-03-05 11:24:16 +0100

    ajout fonction askUser

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+console.log('Jsuis là');
 
 const matchesMin = 1
 const matchesMax = 6
 
-const removeMatches = (nbrOfMatches) => {
 
+const removeMatches = (nbrOfMatchesToRemove) => {
+    if(nbrOfMatchesToRemove > 0) {
+        askUser()
+    } else {
+        return 'Le jeu est terminé'
+    }
 }
 
 
@@ -11,17 +17,17 @@ const askUser = () => {
     let maxMatches = 50   
      // Tant qu'il reste des allumettes dans le tas
     while (maxMatches > 0) {
-        // Demande à l'utilisateur combien d'allumettes il souhaite retirer
-        let askForRemove = parseInt(prompt(`Il reste ${maxMatches} allumettes. Combien d'allumettes voulez-vous retirer ? Min : 1 / Max : 6`));
+        // je demande à l'utilisateur combien d'allumettes il souhaite retirer
+        let askForRemove = parseInt(prompt(`Il reste ${maxMatches} allumettes. Combien d'allumettes voulez-vous retirer ? (Min : 1 / Max : 6)`));
 
-         // Vérifie que l'utilisateur a entré un nombre valide
-         if(askForRemove < 1){
-            return `Nombre minimum ${matchesMin}`
-         } else if (askForRemove > 6){
-            return `Nombre maximum ${matchesMin}`
+         // je vérifie que l'utilisateur a entré un nombre valide [e] 1 et 6
+         if(askForRemove < matchesMin){
+            return `Nombre minimum : ${matchesMin}`
+         } else if (askForRemove > matchesMax){
+            return `Nombre maximum : ${matchesMin}`
          }
     }
-    console.log(index);
 }
+
 
 
